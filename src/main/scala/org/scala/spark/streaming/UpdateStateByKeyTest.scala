6e7b571c037bd8e6a226448e136b17b360942d12
yqq <1037123792@qq.com>
2020-02-27 18:40:49 +0800

    update streaming code

--- src/main/scala/org/scala/spark/streaming/UpdateStateByKeyTest.scala
+++ src/main/scala/org/scala/spark/streaming/UpdateStateByKeyTest.scala
@@ -11,25 +11,25 @@ object UpdateStateByKeyTest {
       .setAppName("UpdateStateByKeyTest")
       .setMaster("local[2]")
 
+    //定义状态更新函数
+    val updateFunc = (values: Seq[Int], state: Option[Int]) => {
+      val currentCount  = values.foldLeft(0)(_ + _)
+      val previousCount  = state.getOrElse(0)
+      Some(currentCount + previousCount)
+    }
+
 
     val ssc = new  StreamingContext(conf, Seconds(3))
 
     // open checkpoint
-    ssc.checkpoint("hdfs://192.168.243:8020/input")
+    ssc.checkpoint("hdfs://192.168.1.243:8020/test")
 
     //连接nc(netcat)服务，接收数据源，产生DStream对象
     val linesDStream = ssc.socketTextStream("192.168.1.248", 9999)
 
     val pairsDStream = linesDStream.flatMap(_.split(" ")).map(word => (word, 1))
 
-    val result = pairsDStream.updateStateByKey((values: Seq[Int], state: Option[Int]) => {
-      val newValue: Int = state.getOrElse(0)
-      // 创建一个变量，用于记录单词出现的次数
-      for (value <- values) {
-        newValue += value
-      }
-      Option(newValue)
-    })
+    val result = pairsDStream.updateStateByKey(updateFunc)
 
     result.print()
 
